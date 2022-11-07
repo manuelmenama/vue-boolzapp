cb840224276d044a936dfbd3ddd66d79a428e134
manuelmenama <manuel.mena090589@gmail.com>
2022-11-07 22:55:59 +0100

    milestone1 complete

--- milestone-1/js/script.js
+++ milestone-1/js/script.js
@@ -32,7 +32,7 @@ createApp({
         {
           name: 'Fabio',
           avatar: '_2',
-          visible: true,
+          visible: false,
           messages: [
             {
               date: '20/03/2020 16:30:00',
@@ -54,7 +54,7 @@ createApp({
         {
           name: 'Samuele',
           avatar: '_3',
-          visible: true,
+          visible: false,
           messages: [
             {
               date: '28/03/2020 10:10:40',
@@ -76,7 +76,7 @@ createApp({
         {
           name: 'Alessandro B.',
           avatar: '_4',
-          visible: true,
+          visible: false,
           messages: [
             {
               date: '10/01/2020 15:30:55',
@@ -93,7 +93,7 @@ createApp({
         {
           name: 'Alessandro L.',
           avatar: '_5',
-          visible: true,
+          visible: false,
           messages: [
             {
               date: '10/01/2020 15:30:55',
@@ -110,7 +110,7 @@ createApp({
         {
           name: 'Claudia',
           avatar: '_6',
-          visible: true,
+          visible: false,
           messages: [
             {
               date: '10/01/2020 15:30:55',
@@ -132,7 +132,7 @@ createApp({
         {
           name: 'Federico',
           avatar: '_7',
-          visible: true,
+          visible: false,
           messages: [
             {
               date: '10/01/2020 15:30:55',
@@ -149,7 +149,7 @@ createApp({
         {
           name: 'Davide',
           avatar: '_8',
-          visible: true,
+          visible: false,
           messages: [
             {
               date: '10/01/2020 15:30:55',
@@ -168,7 +168,7 @@ createApp({
             }
           ],
         }
-        
+      ]
     }
   },
   methods: {
